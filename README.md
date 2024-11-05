beb2c7935c69bd03d9f49b8a99ab0a7fbfa744db
Wouter Jehee <wouter@wouterjehee.com>
2024-11-05 09:32:30 +0100

    readme + some todos

---
+++ README.md
@@ -0,0 +1,11 @@
+# Ramkoers puzzel
+
+We created an escape room for a teambuilding session.
+This code runs one of the puzzles.
+
+## Thanks
+
+- [Rahix for avr-hal and their various posts](https://blog.rahix.de/005-avr-hal-millis/)
+- [Jeroen Vlek, interrupts](https://www.perceptivebits.com/interrupted-by-embedded-rust/)
+- [chrisjpalmer, servos](https://github.com/chrisjpalmer/rust-arduino-servo/tree/main)
+
--- src/main.rs
+++ src/main.rs
@@ -9,12 +9,12 @@ use arduino_hal::{hal::port::Dynamic, port::{mode::Analog, Pin}, Adc};
 fn main() -> ! {
     let p = arduino_hal::Peripherals::take().unwrap();
     let pins = arduino_hal::pins!(p);
+    let mut adc = arduino_hal::Adc::new(p.ADC, Default::default());
     let mut serial = arduino_hal::default_serial!(p, pins, 115200);
 
     let mut led = pins.d13.into_output();
 
-    //let mut adc = arduino_hal::Adc::new(p.ADC, Default::default());
-    //let pot = pins.a5.into_analog_input(&mut adc);
+    let pot = pins.a5.into_analog_input(&mut adc);
 
     //let pot = Pot {
     //    pot,
@@ -22,7 +22,13 @@ fn main() -> ! {
     //};
 
     loop {
-        ufmt::uwriteln!(serial, "\rangle: {}", 10).unwrap();
+        // 0-1023
+        let value = pot.analog_read(&mut adc);
+        // TODO: map to angle in 0-180 range
+        // TODO: make servo work
+
+        ufmt::uwriteln!(serial, "\rangle: {}", value).unwrap();
+
         led.toggle();
         arduino_hal::delay_ms(1000);
     }
